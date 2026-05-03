6ea90c083064cddd3c4e2e3f8de7b8127cb4bdb4
Evtim Kostadinov <evtim.kostadinov2000@gmail.com>
2026-05-03 18:23:22 +0200

    basic movement square

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,62 @@
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Person;
+const PLAYER_SIZE: f32 = 50.0;
+const PLAYER_SPEED: f32 = 400.0;
 
 #[derive(Component)]
-struct Name(String);
+struct Player;
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Hume".to_string();
-            break; // We don't need to change any other names.
-        }
-    }
-}
-
-fn hello_world() {
-    println!("Hello world!");
-}
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
-    }
-}
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .add_systems(Startup, setup)
+        .add_systems(Update, move_player)
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Sprite {
+            color: Color::srgb(0.3, 0.7, 0.9),
+            custom_size: Some(Vec2::splat(PLAYER_SIZE)),
+            ..default()
+        },
+        Transform::from_xyz(0.0, 0.0, 0.0),
+        Player,
+    ));
+}
+
+fn move_player(
+    keys: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    windows: Query<&Window>,
+    mut players: Query<&mut Transform, With<Player>>,
+) {
+    let mut dir = Vec2::ZERO;
+    if keys.pressed(KeyCode::KeyW) || keys.pressed(KeyCode::ArrowUp) {
+        dir.y += 1.0;
+    }
+    if keys.pressed(KeyCode::KeyS) || keys.pressed(KeyCode::ArrowDown) {
+        dir.y -= 1.0;
+    }
+    if keys.pressed(KeyCode::KeyA) || keys.pressed(KeyCode::ArrowLeft) {
+        dir.x -= 1.0;
+    }
+    if keys.pressed(KeyCode::KeyD) || keys.pressed(KeyCode::ArrowRight) {
+        dir.x += 1.0;
+    }
+    let dir = dir.normalize_or_zero();
+
+    let Ok(window) = windows.single() else { return };
+    let half_w = window.width() / 2.0 - PLAYER_SIZE / 2.0;
+    let half_h = window.height() / 2.0 - PLAYER_SIZE / 2.0;
+
+    for mut transform in &mut players {
+        transform.translation.x += dir.x * PLAYER_SPEED * time.delta_secs();
+        transform.translation.y += dir.y * PLAYER_SPEED * time.delta_secs();
+        transform.translation.x = transform.translation.x.clamp(-half_w, half_w);
+        transform.translation.y = transform.translation.y.clamp(-half_h, half_h);
+    }
 }
